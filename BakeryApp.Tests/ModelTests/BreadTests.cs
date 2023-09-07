e01f5ca1dfb8c6a44a8b14aa19a1475bd74fab12
modjordan <mod.jordan@icloud.com>
2023-09-07 12:15:54 -0700

    Initial commit

---
+++ BakeryApp.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BakeryApp.Models;
+
+namespace BakeryApp.Tests
+{
+  [TestClass]
+
+  public class BreadTests
+  {
+    [TestMethod]
+  }
+}
